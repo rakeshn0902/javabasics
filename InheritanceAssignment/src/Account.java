b892d077b0c87d23f619c47db82847735ccfed62
rakeshn0902 <157255406+rakeshn0902@users.noreply.github.com>
2024-01-27 10:35:14 -0600

    Add files via upload

---
+++ InheritanceAssignment/src/Account.java
@@ -0,0 +1,18 @@
+public class Account {
+
+    double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    void withdraw(double amount) {
+    }
+
+    void deposit(double amount) {
+    }
+
+    double getBalance() {
+        return balance;
+    }
+}
